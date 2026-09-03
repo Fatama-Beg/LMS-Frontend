d75d9851f0381fdfdf38ecc702be7b27529fd51c
Fatama beg Tuhin <tuhin5905656@gmail.com>
2026-09-03 22:07:34 +0600

    fix: ignore build type errors and add jszip in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  swcMinify: true,
+  reactStrictMode: true,
   typescript: {
+    // Vercel-এ টাইপস্ক্রিপ্ট এররের কারণে বিল্ড যেন আটকে না যায়
     ignoreBuildErrors: true,
   },
   eslint: {
+    // বিল্ডের সময় লিন্ট সতর্কবার্তা এড়িয়ে যাবে
     ignoreDuringBuilds: true,
   },
-  images: {
-    unoptimized: true,
-  },
+  env: {
+    NEXT_PUBLIC_STRAPI_API_URL: process.env.NEXT_PUBLIC_STRAPI_API_URL || 'https://lms-backend-production-e908.up.railway.app',
+  }
 };
 
 module.exports = nextConfig;
